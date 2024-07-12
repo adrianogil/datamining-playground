91e1a0a7b5a6135ba1a7a339901827af57f39bef
Adriano Gil <adrianogil.san@gmail.com>
2024-07-12 11:09:52 -0400

    feat: implement support for saving downloaded html in a file
    Implement support for saving downloaded html in a file and using it
    instead of download it again. This option allows fast interaction while
    developing a scrape script.

--- dataminingtools/basic/browserdrivertools.py
+++ dataminingtools/basic/browserdrivertools.py
@@ -1,15 +1,21 @@
 
 from selenium import webdriver
 import time
+import os
 
 
-def download_html(url, load_pause_time=5):
+def download_html(url, load_pause_time=5, use_backup_html=None):
     html_content = None
 
-    # Set up the webdriver (this example uses Chrome)
-    browser = webdriver.Chrome()
+    if use_backup_html and os.path.exists(use_backup_html):
+        with open(use_backup_html, 'r', encoding='utf-8') as f:
+            html_content = f.read()
+        return html_content
 
     try:
+        # Set up the webdriver (this example uses Chrome)
+        browser = webdriver.Chrome()
+
         # Navigate to the page
         browser.get(url)
 
@@ -19,6 +25,10 @@ def download_html(url, load_pause_time=5):
 
         # Now the page is fully loaded, get the page source
         html_content = browser.page_source
+
+        if use_backup_html:
+            with open(use_backup_html, 'w', encoding='utf-8') as f:
+                f.write(html_content)
     except Exception as e:
         print("Error occurred while scraping the page: ", e)
         browser.quit()
